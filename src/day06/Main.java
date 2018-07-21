0b06e4fcdabff43cb4c88dbf999642ff25da2302
brandon-fenty <brandonfenty@gmail.com>
2018-07-21 13:43:59 -0700

    adds reverse list method + tests

--- src/day06/Main.java
+++ src/day06/Main.java
@@ -2,6 +2,7 @@ package day06;
 
 public class Main {
     public static void main(String[] args) {
+        // **Main is used for pseudo testing**
 
         ListNode n1 = new ListNode(1);
         ListNode n2 = new ListNode(7);
@@ -13,11 +14,6 @@ public class Main {
         n2.next = n3;
         n3.next = n4;
         n4.next = n5;
-        n5.next = n2;
 
-        LinkedList ll = new LinkedList();
-        ll.root = n1;
-
-        System.out.println(ll.hasLoop());
     }
 }
