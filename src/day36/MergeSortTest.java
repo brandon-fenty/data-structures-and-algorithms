94bd6814cc358b51233a0917117ef05d56abde3c
brandon-fenty <brandonfenty@gmail.com>
2018-08-28 11:56:38 -0700

    adds readme and whiteboard image

--- src/day36/MergeSortTest.java
+++ src/day36/MergeSortTest.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 class MergeSortTest {
 
     @Test
-    void sort() {
+    void happyPath() {
         int[] input = {34, 19, 42, -9, 2018, 0, 2005, 77, 2099};
         System.out.println("Input: " + Arrays.toString(input));
         int[] expected = {-9, 0, 19, 34, 42, 77, 2005, 2018, 2099};
