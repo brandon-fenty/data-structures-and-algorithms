fc3cbbe4d520f594ff8c9e2448debe65f23548c1
brandon-fenty <brandonfenty@gmail.com>
2018-08-29 13:29:15 -0700

    comparing times for sorting functional

---
+++ src/day37/MergeSortDouble.java
@@ -0,0 +1,66 @@
+package day37;
+
+public class MergeSortDouble {
+
+    public static void sort(double[] inputArr, int left, int right) {
+        // Find the middle, left, and right index of the input
+        // and recursively call sort
+        if (left < right) {
+            int middle = (left + right) / 2;
+
+            sort(inputArr, left, middle);
+            sort(inputArr, middle + 1, right);
+
+            merge(inputArr, left, middle, right);
+        }
+    }
+
+    public static void merge(double[] inputArr, int left, int middle, int right) {
+        // Find the middle index of the left and right side of
+        // the input array
+        int lSize = middle - left + 1;
+        int rSize = right - middle;
+        // Create temp arrays to split the input array and store
+        // it's values
+        double[] lArr = new double[lSize];
+        double[] rArr = new double[rSize];
+        // Copy the data from the input array into it's corresponding
+        // temp array
+        for (int i = 0; i < lSize; i++) {
+            lArr[i] = inputArr[left + i];
+        }
+        for (int j = 0; j < rSize; j++) {
+            rArr[j] = inputArr[middle + 1 + j];
+        }
+        // Declare index variables to for left array, right array
+        // and the input array
+        int li = 0;
+        int ri = 0;
+        int ii = left;
+        // Begin merging elements from the left && right arrays
+        // into the original array
+        while (li < lSize && ri < rSize) {
+            if (lArr[li] <= rArr[ri]) {
+                inputArr[ii] = lArr[li];
+                li++;
+            } else {
+                inputArr[ii] = rArr[ri];
+                ri++;
+            }
+            ii++;
+        }
+        // Check to see if there are any remaining elements in the
+        // left or right arrays and add them to the input array
+        while (li < lSize) {
+            inputArr[ii] = lArr[li];
+            li++;
+            ii++;
+        }
+
+        while (ri < rSize) {
+            inputArr[ii] = rArr[ri];
+            ri++;
+            ii++;
+        }
+    }
+}
--- src/day37/SelectionSort.java
+++ src/day37/SelectionSort.java
@@ -1,19 +1,113 @@
 package day37;
 
+import com.sun.scenario.effect.Merge;
+import day36.MergeSort;
+
 import java.util.Arrays;
 import java.util.Random;
 
+import static day37.MergeSortDouble.sort;
+
 public class SelectionSort {
     public static void main(String[] args) {
+        // Array with 10,000 data points
         double[] arr10k = randomN(10000);
-        double[] copy = duplicator(arr10k);
-        System.out.println("Size of original: " + arr10k.length);
-        System.out.println(Arrays.toString(arr10k));
-        System.out.println("Size of copy: " + copy.length);
-        System.out.println(Arrays.toString(copy));
+        double[] copy10k = duplicator(arr10k);
 
-        System.out.println("Sorted: " + Arrays.toString(selectSort(arr10k)));
-        System.out.println("Is it actually sorted? " + isSorted(selectSort(arr10k)));
+        // Array with 100,000 data points
+        double[] arr100k = randomN(100000);
+        double[] copy100k = duplicator(arr100k);
+
+        // Array with 10,000,000 data points
+        double[] arr1mil = randomN(1000000);
+        double[] copy1mil = duplicator(arr1mil);
+
+        // =========================================
+        // ========= 10,000 data points ============
+        // =========================================
+
+//        System.out.println("---- 10,000 data points ----");
+//        System.out.println();
+//        System.out.println("Size of original: " + arr10k.length);
+//        System.out.println();
+//
+//        System.out.println("Size of copy: " + copy10k.length);
+//        System.out.println();
+//
+//        long start10k = System.currentTimeMillis();
+//        selectSort(arr10k);
+//        long delta10k = System.currentTimeMillis() - start10k;
+//
+//        System.out.println("Select sort:");
+//        System.out.println("Is it actually sorted? " + isSorted(arr10k));
+//        System.out.println("Time: " + delta10k + "ms");
+//        System.out.println();
+//
+//        long start10kCopy = System.currentTimeMillis();
+//        sort(copy10k, 0, copy10k.length - 1);
+//        long delta10kCopy = System.currentTimeMillis() - start10kCopy;
+//
+//        System.out.println("Merge sort:");
+//        System.out.println("Is it actually sorted? " + isSorted(copy10k));
+//        System.out.println("Time: " + delta10kCopy + "ms");
+
+        // =========================================
+        // ========= 100,000 data points ===========
+        // =========================================
+
+//        System.out.println("---- 100,000 data points ----");
+//        System.out.println();
+//        System.out.println("Size of original: " + arr100k.length);
+//        System.out.println();
+//
+//        System.out.println("Size of copy: " + copy100k.length);
+//        System.out.println();
+//
+//        long start100k = System.currentTimeMillis();
+//        selectSort(arr100k);
+//        long delta100k = System.currentTimeMillis() - start100k;
+//
+//        System.out.println("Select sort:");
+//        System.out.println("Is it actually sorted? " + isSorted(arr100k));
+//        System.out.println("Select sort time: " + delta100k + "ms");
+//        System.out.println();
+//
+//        long start100kCopy = System.currentTimeMillis();
+//        sort(copy100k, 0, copy100k.length - 1);
+//        long delta100kCopy = System.currentTimeMillis() - start100kCopy;
+//
+//        System.out.println("Merge sort:");
+//        System.out.println("Is it actually sorted? " + isSorted(selectSort(copy100k)));
+//        System.out.println("Merge sort time: " + delta100kCopy + "ms");
+
+        // =========================================
+        // ========= 1,000,000 data points ===========
+        // =========================================
+
+        System.out.println("---- 1,000,000 data points ----");
+        System.out.println();
+        System.out.println("Size of original: " + arr1mil.length);
+        System.out.println();
+
+        System.out.println("Size of copy: " + copy1mil.length);
+        System.out.println();
+
+        long start1mil = System.currentTimeMillis();
+        selectSort(arr1mil);
+        long delta1mil = System.currentTimeMillis() - start1mil;
+
+        System.out.println("Select sort:");
+        System.out.println("Is it actually sorted? " + isSorted(arr1mil));
+        System.out.println("Select sort time: " + delta1mil + "ms");
+        System.out.println();
+
+        long start1milCopy = System.currentTimeMillis();
+        sort(copy1mil, 0, copy1mil.length - 1);
+        long delta1milCopy = System.currentTimeMillis() - start1milCopy;
+
+        System.out.println("Merge sort:");
+        System.out.println("Is it actually sorted? " + isSorted(selectSort(copy1mil)));
+        System.out.println("Merge sort time: " + delta1milCopy + "ms");
     }
 
     public static double[] selectSort(double[] input) {
